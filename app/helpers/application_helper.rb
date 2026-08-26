417b0e03bfce99105b6025727cdab8a4a19a3ede
Luf3r <luf3developer@proton.me>
2026-08-25 23:21:01 -0400

    feat: localizar estados e auditoria de pagamentos (#131)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def payment_status_label(status)
+    { "reported" => "declarado", "confirmed" => "confirmado", "cancelled" => "cancelado" }.fetch(status.to_s)
+  end
 end
